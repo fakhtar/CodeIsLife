7b76d28299434998213f6ad2e3b63f05773fddb4
fakhtar <faisalakhtar@yahoo.com>
2020-04-13 21:16:16 -0400

    Video 11

--- Video 11/userinput.py
+++ Video 11/userinput.py
@@ -1,1 +1,4 @@
 enterd_name = input('What is your name?')
+print('Hello ' + enterd_name)
+enterd_age = input('How old are you ' + enterd_name +'?')
+print('You have ' + str(100-int(enterd_age)) + ' years untill your 100th birthday.')
