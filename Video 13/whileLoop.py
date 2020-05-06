8966be3f472b21949f29883c9eedac316ae152d2
fakhtar <faisalakhtar@yahoo.com>
2020-05-06 15:39:09 -0400

    Video 13

---
+++ Video 13/whileLoop.py
@@ -0,0 +1,5 @@
+input_provided = input ("What is your name?")
+while input_provided != 'quit':
+    print('Hello ' + input_provided)
+    input_provided = input ("What is your name?")
+git 
---
+++ insertionSort.py
@@ -0,0 +1,11 @@
+A = [3,1,5,6,1,7,8,2,3,8,6]
+
+for j in range(1,len(A)):
+    key = A[j]
+    i = j-1
+    while i >= 0 and A[i] >key:
+        A[i+1] = A[i]
+        i=i-1
+    A[i+1] = key
+
+print(A)
