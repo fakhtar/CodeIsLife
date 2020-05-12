cfd3986f58239f9fb11540ad8eef13e82f6ef667
fakhtar <faisalakhtar@yahoo.com>
2020-05-12 15:11:44 -0400

    Video 13

---
+++ Video 14/boolean.py
@@ -0,0 +1,12 @@
+x = 1
+y = 2
+if x < 1:
+    print('x is less than 1')
+if y >= 1:
+    print('y is greater than or equal to 1')
+if x == 0:
+    print('X is 0')
+if x == 0 or y ==2:
+    print('X is 0 or y is 2')
+if x == 0 and y ==2:
+    print('X is 0 and y is 2')
