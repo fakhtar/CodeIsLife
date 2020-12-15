33c21e798129b70cb76be2db83b67c06229753ac
Faisal Akhtar <faisalakhtar@yahoo.com>
2020-12-14 20:12:18 -0500

    Video 21

--- Video 21/exceptionhandling.py
+++ Video 21/exceptionhandling.py
@@ -2,7 +2,7 @@ try:
     f = open('testfile.txt')
 except FileNotFoundError as e:
     print(e)
-    print('This is where other code would go if the does not exist.')
+    print('This is where other code would go if the file does not exist.')
 else:
     print(f.read())
     f.close()
